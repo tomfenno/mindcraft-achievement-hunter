bb9b1763241d52e5a44d06723f166b29f055d04f
tomfenno <treyfenno@gmail.com>
2026-03-20 13:57:05 -0600

    setting up test files to change agent behavior.

--- src/process/init_agent.js
+++ src/process/init_agent.js
@@ -1,4 +1,4 @@
-import { Agent } from '../agent/agent.js';
+import { Agent } from '../agent/agent_copy.js';
 import { serverProxy } from '../agent/mindserver_proxy.js';
 import yargs from 'yargs';
 
